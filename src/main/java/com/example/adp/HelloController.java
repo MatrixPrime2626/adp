681945a91490bd6b5e5203d8c13feb6f1a06b019
MatrixPrime2626 <djratnam26@gmail.com>
2026-04-06 20:43:07 +0100

    1

--- src/main/java/com/example/adp/HelloController.java
+++ src/main/java/com/example/adp/HelloController.java
@@ -4,16 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.SortedSet;
-import java.util.TreeMap;
+import java.util.*;
 
 @RestController
 class HelloController {
 
 
-    private final TreeMap<Long, MyPOJO> map = new TreeMap<>();
+   // private final TreeMap<Long, MyPOJO> map = new TreeMap<>();
 
+    private final PojoRepository pojoRepository;
+    private final PostLogRepository postLogRepository;
+
+    public HelloController(PojoRepository pojoRepository, PostLogRepository postLogRepository) {
+        this.pojoRepository = pojoRepository;
+        this.postLogRepository = postLogRepository;
+    }
     @GetMapping("/")
     public String hello() {
         return "hello world!";
@@ -24,30 +29,56 @@ class HelloController {
         return new String[]{"hello world!", "again", "hang on what's this?", "ladle", "nap", "stoop"};
     }
 
+//    @GetMapping("/pojo")
+//    public Collection<MyPOJO> getAll() {
+//        return map.values();
+//    }
+
     @GetMapping("/pojo")
     public Collection<MyPOJO> getAll() {
-        return map.values();
+        return this.pojoRepository.findAll();
     }
 
+//    @GetMapping("/pojo/{id}")
+//    public MyPOJO getById(@PathVariable("id") Long id) {
+//        MyPOJO item = map.get(id);
+//        if (item == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id);
+//        } else {
+//            return item;
+//        }
+//    }
     @GetMapping("/pojo/{id}")
     public MyPOJO getById(@PathVariable("id") Long id) {
-        MyPOJO item = map.get(id);
-        if (item == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id);
-        } else {
-            return item;
-        }
+        return this.pojoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id));
     }
+
+//    @PostMapping("/pojo")
+//    public MyPOJO postPojo(@RequestBody MyPOJO body) {
+//        long nextId = 1;
+//        if (!this.map.isEmpty()) {
+//            nextId = ((SortedSet<Long>) this.map.keySet()).last() + 1;
+//        }
+//        MyPOJO newItem = new MyPOJO(body.getFirstName(), body.getLastName(), nextId);
+//        this.map.put(nextId, newItem);
+//        return newItem;
+//    }
 
     @PostMapping("/pojo")
     public MyPOJO postPojo(@RequestBody MyPOJO body) {
-        long nextId = 1;
-        if (!this.map.isEmpty()) {
-            nextId = ((SortedSet<Long>) this.map.keySet()).last() + 1;
+        body.setIdNumber(0); // clear any ID number to prevent update
+        MyPOJO pojo = this.pojoRepository.save(body);
+        this.postLogRepository.save(new PostLog(pojo.getIdNumber()));
+        return pojo;
+    }
+
+    @PostMapping("pojos")
+    public List<MyPOJO> postABunch(@RequestBody List<MyPOJO> list) {
+        List<MyPOJO> returnList = new ArrayList<>(list.size());
+        for(MyPOJO pojo : list) {
+            returnList.add(postPojo(pojo));
         }
-        MyPOJO newItem = new MyPOJO(body.getFirstName(), body.getLastName(), nextId);
-        this.map.put(nextId, newItem);
-        return newItem;
+        return returnList;
     }
 
     @GetMapping("/pojo/{first}/{last}/{id}")
@@ -56,18 +87,36 @@ class HelloController {
                          @PathVariable("id") int idNumber) {
         return new MyPOJO(firstName, lastName, idNumber);
     }
+//    @PutMapping("/pojo")
+//    public MyPOJO putPojo(@RequestBody MyPOJO body) {
+//        this.map.put(body.getIdNumber(), body);
+//        return body;
+//    }
+
     @PutMapping("/pojo")
     public MyPOJO putPojo(@RequestBody MyPOJO body) {
-        this.map.put(body.getIdNumber(), body);
-        return body;
+        return this.pojoRepository.save(body);
     }
 
+//    @DeleteMapping("/pojo/{id}")
+//    public String[] deletePojo(@PathVariable long id) {
+//        if (this.map.remove(id) == null) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id);
+//        } else {
+//            return new String[] {"item removed"};
+//        }
+//    }
     @DeleteMapping("/pojo/{id}")
     public String[] deletePojo(@PathVariable long id) {
-        if (this.map.remove(id) == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id);
-        } else {
+        if (this.pojoRepository.existsById(id)) {
+            this.pojoRepository.deleteById(id);
             return new String[] {"item removed"};
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such id " + id);
         }
+    }
+    @GetMapping("/postlogs")
+    public Collection<PostLog> getAllPostLogs() {
+        return this.postLogRepository.findAll();
     }
 }
